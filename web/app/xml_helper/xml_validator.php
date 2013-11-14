20e85ae5ae1b6c51cf37d6458803788b58e30220
Shaheel Caunhye <shaheel612@hotmail.com>
2013-11-14 17:19:08 +0100

    xml validator

---
+++ web/app/xml_helper/xml_validator.php
@@ -0,0 +1,24 @@
+<?php
+
+header ('Content-type: text/html; charset=utf-8');
+
+function validate_xml_file($dtdFilePath,$xmlFilePath,$rootTag)
+{
+    $xml = file_get_contents($xmlFilePath);
+    
+    $old = new DOMDocument;
+    $old->loadXML($xml);
+
+    $creator = new DOMImplementation;
+    $doctype = $creator->createDocumentType($rootTag, null, 'user.example.dtd');
+    $new = $creator->createDocument(null, null, $doctype);
+    $new->encoding = "utf-8";
+
+    $oldNode = $old->getElementsByTagName($rootTag)->item(0);
+    $newNode = $new->importNode($oldNode, true);
+    $new->appendChild($newNode);
+
+    return $new->validate();
+}
+
+?>
